96146641e590413372e88516997be2e448169475
leogjhuang <77544794+leogjhuang@users.noreply.github.com>
2021-12-23 18:19:46 -0700

    added bottle model

---
+++ models/bottle.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const BottleSchema = new Schema({
+  accountId: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  editDate: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  tags: [{
+    type: String,
+    required: true,
+  }],
+  views: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = Item = mongoose.model("bottle", BottleSchema);
