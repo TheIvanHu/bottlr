78fb0b98fec32d0ce5e7494f8bacd072f5a7b2f2
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2021-12-26 15:02:06 -0700

    resolved port error

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5500;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
