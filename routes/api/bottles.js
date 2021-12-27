a8e2e71562eb9d98642aeb32950c1f80c7882324
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2021-12-27 10:45:04 -0700

    added error messages and bottle editing

--- routes/api/bottles.js
+++ routes/api/bottles.js
@@ -5,17 +5,31 @@ const router = express.Router();
 const Bottle = require("../../models/Bottle");
 
 // @route   GET api/bottles
-// @desc    Get Bottle(s)
+// @desc    Get All Bottles
 // @access  Public
 router.get("/", (req, res) => {
-  if (req.query.id)
-    Bottle.findById(req.query.id)
-      .then((items) => res.json(items))
-      .catch(() => res.status(404).json({ success: false }));
-  else
-    Bottle.find()
-      .then((items) => res.json(items))
-      .catch(() => res.status(404).json({ success: false }));
+  Bottle.find()
+    .then((items) => res.json(items))
+    .catch(() => res.status(500).json({ msg: "Error", success: false }));
+});
+
+// @route   GET api/bottles/:bottleId
+// @desc    Get A Bottle
+// @access  Public
+router.get("/:bottleId", (req, res) => {
+  Bottle.findById(req.params.bottleId)
+    .then((bottle) => {
+      if (bottle == null)
+        res.status(404).json({ msg: "Bottle not found", success: false });
+      else res.json(bottle);
+    })
+    .catch((err) => {
+      if (err.path == "_id")
+        res
+          .status(400)
+          .json({ msg: "Bottle ID not properly formatted", success: false });
+      else res.status(500).json({ msg: "Error", success: false });
+    });
 });
 
 // @route   POST api/bottles
@@ -23,7 +37,9 @@ router.get("/", (req, res) => {
 // @access  Public
 router.post("/", (req, res) => {
   if (!req.body.accountId || !req.body.country) {
-    return res.status(400).json({ success: false });
+    return res
+      .status(400)
+      .json({ msg: "Need account ID and country", success: false });
   }
   const newBottle = new Bottle({
     accountId: req.body.accountId,
@@ -36,16 +52,49 @@ router.post("/", (req, res) => {
   newBottle
     .save()
     .then((bottle) => res.json(bottle))
-    .catch(() => res.status(500).json({ success: false }));
+    .catch(() => res.status(500).json({ msg: "Error", success: false }));
 });
 
-// @route   DELETE api/Bottles
+// @route   POST api/bottles/:bottleId/edit
+// @desc    Edit A Bottle
+// @access  Public
+router.post("/:bottleId/edit", (req, res) => {
+  if (!req.params.bottleId) {
+    return res.status(400).json({ msg: "Need bottle ID", success: false });
+  }
+  Bottle.findById(req.params.bottleId).then((bottle) => {
+    if (bottle == null)
+      res.status(404).json({ msg: "Bottle not found", success: false });
+    else {
+      if (req.body.editDate) bottle.editDate = req.body.editDate;
+      if (req.body.message) bottle.message = req.body.message;
+      if (req.body.tags) bottle.tags = req.body.tags;
+      if (req.body.views) bottle.views = req.body.views;
+      bottle
+        .save()
+        .then((bottle) => res.json(bottle))
+        .catch(() => res.status(500).json({ msg: "Error", success: false }));
+    }
+  });
+});
+
+// @route   DELETE api/Bottles/:bottleId
 // @desc    Delete A Bottle
 // @access  Public
-router.delete("/", (req, res) => {
-  Bottle.findById(req.query.id)
-    .then((bottle) => bottle.remove().then(() => res.json({ success: true })))
-    .catch(() => res.status(404).json({ success: false }));
+router.delete("/:bottleId", (req, res) => {
+  Bottle.findById(req.params.bottleId)
+    .then((bottle) => {
+      if (bottle == null)
+        res.status(404).json({ msg: "Bottle not found", success: false });
+      else bottle.remove().then(() => res.json({ success: true }));
+    })
+    .catch((err) => {
+      if (err.path == "_id")
+        res
+          .status(400)
+          .json({ msg: "Bottle ID not properly formatted", success: false });
+      else res.status(500).json({ msg: "Error", success: false });
+    });
 });
 
 module.exports = router;
