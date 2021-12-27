6dd7ba25aa33a5c5f4f8b50b1a352197024f0ed1
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2021-12-26 19:56:10 -0700

    added error handling

--- routes/api/bottles.js
+++ routes/api/bottles.js
@@ -5,21 +5,26 @@ const router = express.Router();
 const Bottle = require("../../models/Bottle");
 
 // @route   GET api/bottles
-// @desc    Get A Bottle
+// @desc    Get Bottle(s)
 // @access  Public
 router.get("/", (req, res) => {
-  Bottle.find()
-    .then(items => res.json(items))
-  
-  // Bottle.findById(req.body.id)
-  //   .then((bottle) => res.json(bottle))
-  //   .catch(() => res.status(404).json({ success: false }));
+  if (req.query.id)
+    Bottle.findById(req.query.id)
+      .then((items) => res.json(items))
+      .catch(() => res.status(404).json({ success: false }));
+  else
+    Bottle.find()
+      .then((items) => res.json(items))
+      .catch(() => res.status(404).json({ success: false }));
 });
 
 // @route   POST api/bottles
 // @desc    Create A Bottle
 // @access  Public
 router.post("/", (req, res) => {
+  if (!req.body.accountId || !req.body.country) {
+    return res.status(400).json({ success: false });
+  }
   const newBottle = new Bottle({
     accountId: req.body.accountId,
     country: req.body.country,
@@ -28,14 +33,17 @@ router.post("/", (req, res) => {
     tags: req.body.tags,
     views: req.body.views,
   });
-  newBottle.save().then((bottle) => res.json(bottle));
+  newBottle
+    .save()
+    .then((bottle) => res.json(bottle))
+    .catch(() => res.status(500).json({ success: false }));
 });
 
 // @route   DELETE api/Bottles
 // @desc    Delete A Bottle
 // @access  Public
 router.delete("/", (req, res) => {
-  Bottle.findById(req.body.id)
+  Bottle.findById(req.query.id)
     .then((bottle) => bottle.remove().then(() => res.json({ success: true })))
     .catch(() => res.status(404).json({ success: false }));
 });
