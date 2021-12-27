8a83705a9af6cf26d2e8aee1f52c850c0477614f
Kristy <kristy.lau@uwaterloo.ca>
2021-12-27 11:24:59 -0500

    set background in home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,41 +1,41 @@
 import React, { Component } from 'react';
 import { Container, Button } from 'reactstrap';
+import background from "../assets/background.jpg"
 import AppNavbar from './AppNavbar';
 
 class Home extends Component {
   render(){
     return(
-      <Container >
-        <div style = {{
-          position: "fixed",
-          top: "0px",
-          left: "0px",
-          zIndex: "1002",
-          minWidth: "100%",
-          minHeight: "100%",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignitems: "center",
-          backgroundColor: "white",
-        }}>
-
-          
+      <div style = {{ 
+        backgroundImage: `url(${background})`,  
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        position: "fixed",
+        top: "0px",
+        left: "0px",
+        zIndex: "1002",
+        minWidth: "100%",
+        minHeight: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignitems: "center",
+        backgroundColor: "white",
+      }}>
+  
         <div
+          class = "center"
           style={{
-
             position: "absolute",
-            top: "center",
-            left:"30vw",
             zIndex: "1",
             display: "flex",
             flexDirection: "column",
-            alignItems: "center",
             padding: "10vh 10vw",
             borderRadius: "15vh",
-            backdropFilter: 'blur(10vh)'
+            backdropFilter: 'blur(2vh)'
           }}
-        >
+          >
           <main class="px-3">
           <h1 style={{ color: "black", fontSize: "3rem" }}>
             Bottlr{" "}
@@ -44,11 +44,9 @@ class Home extends Component {
                 and read from others around the world</p>
           <a href="/feed" class="btn btn-lg btn-secondary font-weight-bold border-black bg-white text-black">Enter and Start Browsing</a>
           </main>
-          
+            
         </div>
       </div>
-      </Container>
-      
     )
   }
 }
